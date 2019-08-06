c9f6b026b044f58b54c39a7795b5d4aef5f3343c
ivescai <caij1@rose-hulman.edu>
2019-08-06 20:55:09 +0800

    done

---
+++ libs/turtle_warrior.py
@@ -0,0 +1,13 @@
+
+import rosegraphics as rg
+import random
+
+
+class Warrior(object):
+
+    def __init__(self):
+        self.hp = 10
+        self.mp = 10
+        self.lv = 1
+        self.exp = 0
+        self.atk = 1
