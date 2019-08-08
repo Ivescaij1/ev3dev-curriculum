270a643f24039ef70b0cbe10a1220952c472a878
ivescai <caij1@rose-hulman.edu>
2019-08-08 19:20:01 +0800

    noyhing done

--- projects/caij1/gui_control.py
+++ projects/caij1/gui_control.py
@@ -9,30 +9,33 @@ def main():
     root.title("Game Control")
 
     # grid(0, 1-3) & (1-3, 0): Empty frame that holds left and right buttons frame always in same size
-    # TODO: check width after button is done
-    top_bar_left = tk.Frame(root, width=300, height=0)
+    top_bar_left = tk.Frame(root, width=280, height=0)
     top_bar_left.grid(row=0, column=1)
     top_bar_right = ttk.Frame(root, width=1080, height=0)
     top_bar_right.grid(row=0, column=2)
-    top_bar_right = ttk.Frame(root, width=300, height=0)
+    top_bar_right = ttk.Frame(root, width=280, height=0)
     top_bar_right.grid(row=0, column=3)
 
     side_bar_top = tk.Frame(root, width=0, height=75)
     side_bar_top.grid(row=1, column=0)
+    side_bar_middle = tk.Frame(root, width=0, height=480)
+    side_bar_middle.grid(row=2, column=0)
+    side_bar_middle = tk.Frame(root, width=0, height=240)
+    side_bar_middle.grid(row=3, column=0)
 
     # grid(1,1): TOP left frame that contain the position label and entry box
-    # 3 column, 2 row used, with one free column/row for stikcy room
+    # 3 column, 2 row used, with one free column/row for sticky room
     position_frame = tk.Frame(root, bg='orange red')
     position_frame.grid(row=1, column=1, sticky="nsew")
     position_frame.grid_rowconfigure([0, 3], weight=1)
     position_frame.grid_columnconfigure([0, 4], weight=1)
 
     position_label = tk.Label(position_frame, text="Position", bg='orange red')
-    position_label.grid(row=1, column=2)
+    position_label.grid(row=2, column=2)
     x_entry = ttk.Entry(position_frame, width=8, justify=tk.CENTER)
-    x_entry.grid(row=2, column=1)
+    x_entry.grid(row=1, column=1)
     y_entry = ttk.Entry(position_frame, width=8, justify=tk.CENTER)
-    y_entry.grid(row=2, column=3)
+    y_entry.grid(row=1, column=3)
 
     # grid(1,2): character button, map button, start button
     # 3 column, 1 row used, with space between buttons and perimeter [row 0 & 2, column 0 & 2 & 4 & 6] expandable.
@@ -64,9 +67,9 @@ def main():
     direction_frame.grid_columnconfigure([0, 2], weight=1)
 
     direction_label = tk.Label(direction_frame, text="Direction", bg='dodger blue')
-    direction_label.grid(row=1, column=1)
+    direction_label.grid(row=2, column=1)
     direction_entry = ttk.Entry(direction_frame, width=8, justify=tk.CENTER)
-    direction_entry.grid(row=2, column=1)
+    direction_entry.grid(row=1, column=1)
 
     # grid(2, 2) Canvas display frame with tkinter canvas, free grid around perimeter.
     canvas_frame = tk.Frame(root, bg='black')
@@ -88,72 +91,117 @@ def main():
     textbox.grid(row=1, column=1)
     textbox.tag_configure("center", justify='center')
 
+    # grid(2, 1) left move control, free grid around perimeter and between 'move' and direction button
+    # * have row0 reserved even if not expandable
+    move_frame = tk.Frame(root, bg='orange red')
+    move_frame.grid(row=2, column=1, sticky='nsew')
+    move_frame.grid_rowconfigure([2, 6], weight=1)
+    move_frame.grid_columnconfigure([0, 4], weight=1)
 
+    move_button = tk.Button(move_frame, text="Move", height=2, width=8)
+    move_button.grid(row=1, column=2)
+    move_button['command'] = lambda: print("Move button")
 
-
-
-
-
-
-
-
-    main_frame = ttk.Frame(root, padding=50)
-    main_frame.grid(row=4, column=2, sticky='nswe')
-    main_frame.grid_rowconfigure(1, weight=1)
-    main_frame.grid_columnconfigure(1, weight=1)
-
-
-
-
-
-
-    forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid(row=2, column=1)
-    forward_button['command'] = lambda: print("Forward button")
+    w_button = tk.Button(move_frame, text="W", height=4, width=8)
+    w_button.grid(row=3, column=2)
+    w_button['command'] = lambda: print("Forward button")
     root.bind('<w>', lambda event: print("Forward key"))
 
-    left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid(row=3, column=0)
-    left_button['command'] = lambda: print("Left button")
+    a_button = tk.Button(move_frame, text="A", height=4, width=8)
+    a_button.grid(row=4, column=1)
+    a_button['command'] = lambda: print("Left button")
     root.bind('<a>', lambda event: print("Left key"))
 
-    stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid(row=3, column=1)
+    stop_button = tk.Button(move_frame, text="Stop", height=4, width=8)
+    stop_button.grid(row=4, column=2)
     stop_button['command'] = lambda: print("Stop button")
     root.bind('<space>', lambda event: print("Stop key"))
 
-    right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid(row=3, column=2)
-    right_button['command'] = lambda: print("Right button")
+    d_button = tk.Button(move_frame, text="D", height=4, width=8)
+    d_button.grid(row=4, column=3)
+    d_button['command'] = lambda: print("Right button")
     root.bind('<d>', lambda event: print("Right key"))
 
-    back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid(row=4, column=1)
-    back_button['command'] = lambda: print("Back button")
+    s_button = tk.Button(move_frame, text="S", height=4, width=8)
+    s_button.grid(row=5, column=2)
+    s_button['command'] = lambda: print("Back button")
     root.bind('<s>', lambda event: print("Back key"))
 
-    up_button = ttk.Button(main_frame, text="Up")
-    up_button.grid(row=5, column=0)
+    # grid(2, 3) Right action control
+
+    action_frame = tk.Frame(root, bg='dodger blue')
+    action_frame.grid(row=2, column=3, sticky='nsew')
+    action_frame.grid_rowconfigure([2, 4, 6, 8], weight=1)
+    action_frame.grid_columnconfigure([0, 4], weight=1)
+
+    turn_button = tk.Button(action_frame, text="Turn", height=2, width=8)
+    turn_button.grid(row=1, column=2)
+    turn_button['command'] = lambda: print("Turn button")
+
+    att_button = tk.Button(action_frame, text="Attack \n<J>", height=4, width=8)
+    att_button.grid(row=3, column=2)
+    att_button['command'] = lambda: print("Att button")
+    root.bind('<j>', lambda event: print("Att key"))
+
+    skill_button = tk.Button(action_frame, text="Fire Ball \n<K>", height=4, width=8)
+    skill_button.grid(row=5, column=2)
+    skill_button['command'] = lambda: print("d Att button")
+    root.bind('<k>', lambda event: print("d Att key"))
+
+    rest_button = tk.Button(action_frame, text="Rest \n<L>", height=4, width=8)
+    rest_button.grid(row=7, column=2)
+    rest_button['command'] = lambda: print("rest button")
+    root.bind('<l>', lambda event: print("rest key"))
+
+    # grid (3, 1) hp/mp & exit display frame
+    exit_frame = tk.Frame(root, bg='orange red')
+    exit_frame.grid(row=3, column=1, sticky='nsew')
+    exit_frame.grid_rowconfigure([0, 2, 4], weight=1)
+    exit_frame.grid_columnconfigure([1, 4], weight=1)
+
+    hp_label = tk.Label(exit_frame, text="HP", bg='orange red')
+    hp_label.grid(row=1, column=2)
+    hp = ttk.Entry(exit_frame, width=8, justify=tk.CENTER)
+    hp.grid(row=1, column=3)
+
+    mp_label = tk.Label(exit_frame, text="MP", bg='orange red')
+    mp_label.grid(row=3, column=2)
+    mp = ttk.Entry(exit_frame, width=8, justify=tk.CENTER)
+    mp.grid(row=3, column=3)
+
+    exit_button = ttk.Button(exit_frame, text="Exit <X>")
+    exit_button.grid(row=5, column=0)
+    exit_button['command'] = lambda: exit()
+    root.bind('x', lambda event: exit())
+
+    # grid (3, 3) action control frame
+    turn_frame = tk.Frame(root, bg='dodger blue')
+    turn_frame.grid(row=3, column=3, sticky='nsew')
+    turn_frame.grid_rowconfigure([0, 4], weight=1)
+    turn_frame.grid_columnconfigure([0, 4], weight=1)
+
+    left_button = tk.Button(turn_frame, text="Left <Q>", height=4, width=8)
+    left_button.grid(row=2, column=1)
+    left_button['command'] = lambda: print("left button")
+    root.bind('<q>', lambda event: print("left key"))
+
+    right_button = tk.Button(turn_frame, text="Right <E>", height=4, width=8)
+    right_button.grid(row=2, column=3)
+    right_button['command'] = lambda: print("right button")
+    root.bind('<e>', lambda event: print("right key"))
+
+    up_button = tk.Button(turn_frame, text="Up", height=4, width=8)
+    up_button.grid(row=1, column=2)
     up_button['command'] = lambda: print("Up button")
     root.bind('<u>', lambda event: print("Up key"))
 
-    down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid(row=6, column=0)
+    down_button = tk.Button(turn_frame, text="Down", height=4, width=8)
+    down_button.grid(row=3, column=2)
     down_button['command'] = lambda: print("Down button")
     root.bind('<j>', lambda event: print("Down key"))
 
-    # Buttons for quit and exit
-    q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=5, column=2)
-    q_button['command'] = lambda: print("Quit button")
-    root.bind('c', lambda event: print("Quit key"))
-
-    e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=6, column=2)
-    e_button['command'] = lambda: exit()
-
     def text_director(inputs):
-        textbox.insert('0.1', inputs, 'center')
+        textbox.insert('1.0', inputs, 'center')
 
     sys.stdout.write = text_director  # whenever sys.stdout.write is called, redirector is called.
     root.grid_rowconfigure([1, 2, 3], weight=1)
